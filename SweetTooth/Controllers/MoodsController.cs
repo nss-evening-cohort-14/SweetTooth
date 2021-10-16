62ada13320e6ceb25286f16f587afbbf92a9d2db
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-10-16 12:42:05 -0500

    Set up mood stuff - Models folder - DataAccess folder - delete weather files

---
+++ SweetTooth/Controllers/MoodsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SweetTooth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoodsController : ControllerBase
+    {
+    }
+}
