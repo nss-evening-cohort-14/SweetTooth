9d8a057b4e2c78607a4249341f09b3d1973571fe
jim-conner <jamesdavidconner@gmail.com>
2021-10-17 12:58:46 -0500

    building out user repo

--- SweetTooth/DataAccess/UserRepo.cs
+++ SweetTooth/DataAccess/UserRepo.cs
@@ -2,10 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using SweetTooth.Models;
+//using Dapper;
+
 
 namespace SweetTooth.DataAccess
 {
     public class UserRepo
     {
+        readonly string _connectionString;
+
+        public UserRepo(IConfiguration config)
+        {
+            _connectionString = config.GetConnectionString("SweetTooth");
+        }
+
+        internal void Add(User newUser)
+        {
+            //using var db = new SqlConnection(_connectionString);
+            var sql = @"";
+        }
     }
 }
