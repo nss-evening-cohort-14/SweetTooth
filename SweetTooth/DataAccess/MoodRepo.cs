b54fcb235170d2ec6363566cb105593cfcb3803f
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-10-18 18:41:17 -0500

    Added Microsoft.Data.SqlClient package

--- SweetTooth/DataAccess/MoodRepo.cs
+++ SweetTooth/DataAccess/MoodRepo.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using SweetTooth.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,16 @@ namespace SweetTooth.DataAccess
 {
     public class MoodRepo
     {
+        string _connectionString;
+
+        public MoodRepo(IConfiguration config)
+        {
+            _connectionString = config.GetConnectionString("SweetTooth");
+        }
+
+        internal IEnumerable<Mood> GetAll()
+        {
+            using var db = new SqlConnection(_connectionString);
+        }
     }
 }
