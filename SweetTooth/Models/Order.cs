e0c1dba8346064bc0efd241ce882633f81185618
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-10-19 16:08:47 -0500

    created order model

---
+++ SweetTooth/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SweetTooth.Models
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int OrderNumber { get; set; }
+        public int Total { get; set; }
+        public Guid PaymentMethodId { get; set; }
+        public bool Processed { get; set; }
+        public bool Shipped { get; set; }
+    }
+}
