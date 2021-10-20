4f4fd3311dcec3600a730296d1e6f8410ba88ca2
jim-conner <jamesdavidconner@gmail.com>
2021-10-19 21:04:38 -0500

    Added userAddr model

---
+++ SweetTooth/Models/UserAddress.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SweetTooth.Models
+{
+    public class UserAddress
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+
+    }
+}
