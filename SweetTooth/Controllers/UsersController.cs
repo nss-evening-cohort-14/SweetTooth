9d26898fd400612adbcc94f44b690a43e4ec7e8f
jim-conner <jamesdavidconner@gmail.com>
2021-10-19 21:23:59 -0500

    working on add user w address

--- SweetTooth/Controllers/UsersController.cs
+++ SweetTooth/Controllers/UsersController.cs
@@ -5,8 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SweetTooth.DataAccess;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace SweetTooth.Models
 {
     [Route("api/[controller]")]
@@ -33,7 +31,7 @@ namespace SweetTooth.Models
 
             if (singleUser == null)
             {
-                return NotFound($"No user found with this  ID: {id}.");
+                return NotFound($"No user found with this ID: {id}.");
             }
 
             return Ok(singleUser);
