5d2894da1271e651192ccf3cb75e18104b76b5b3
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-11-01 21:10:46 -0500

    created shopping Page divs, scss, and did some styling/background colors

--- SweetTooth.ui/src/components/ShoppingPage.js
+++ SweetTooth.ui/src/components/ShoppingPage.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Container } from 'reactstrap';
+import '../styles/shoppingPage.scss';
 
 export default function ShoppingPage() {
   return (
-    <div>
-      <h1>Shop for snacks</h1>
-    </div>
+    <>
+      <div className="shoppingPage d-flex flex-column justify-content-around mainDiv">
+        <Container className = "rounded mb-20 border border-primary">
+          <h1>Suggested Snacks (Filtered by Mood)</h1>
+        </Container>
+        <Container className = "rounded mb-20 border border-secondary flex-grow-1 flex-fill">
+          <h1>All Snacks (Filtered by Category</h1>
+        </Container>
+        <Container className = "rounded mb-20 border border-success">
+          <h1>Stretch: Recently Viewed</h1>
+        </Container>
+      </div>
+    </>
   );
 }
