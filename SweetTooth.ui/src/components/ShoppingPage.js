87942a674958f6bdcfec0fe1a2f3826555ba7c83
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-11-02 20:49:26 -0500

    saved shoppingPage

--- SweetTooth.ui/src/components/ShoppingPage.js
+++ SweetTooth.ui/src/components/ShoppingPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container } from 'reactstrap';
 import '../styles/shoppingPage.scss';
+import SnackCard from './SnackCard';
 
 export default function ShoppingPage() {
   return (
@@ -8,6 +9,7 @@ export default function ShoppingPage() {
       <div className="shoppingPage d-flex flex-column justify-content-around mainDiv">
         <Container className = "rounded mb-20 border border-primary m-3">
           <h1>Suggested Snacks (Filtered by Mood)</h1>
+          <SnackCard></SnackCard>
         </Container>
         <Container className = "rounded mb-20 border border-secondary flex-grow-1 flex-fill m-3">
           <h1>All Snacks (Filtered by Category</h1>
