f4eb6a1110db1dbd1829dc71761c57cf05c14531
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-11-06 12:28:21 -0500

    added h2 tags to admin page

--- SweetTooth.ui/src/components/AdminDashboard.js
+++ SweetTooth.ui/src/components/AdminDashboard.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+// need to pass in user/admin to display greeting
+
 export default function AdminDashboard() {
   return (
     <div>
       <h1>Admin Dashboard</h1>
+      <h2>Orders</h2>
+      <h2>Moods</h2>
+      <h2>Snacks</h2>
+      <h2>Users</h2>
     </div>
   );
 }
