ffe49d4fb1fdd4d1350b4776296e973017bfc1c8
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-11-07 17:23:30 -0600

    fleshing out admin page

--- SweetTooth.ui/src/components/AdminDashboard.js
+++ SweetTooth.ui/src/components/AdminDashboard.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import Orders from '../helpers/views/Orders';
+import Moods from '../helpers/views/Moods';
+import Snacks from '../helpers/views/Snacks';
+import Users from '../helpers/views/Users';
 
 // need to pass in user/admin to display greeting
 
@@ -7,9 +11,13 @@ export default function AdminDashboard() {
     <div>
       <h1>Admin Dashboard</h1>
       <h2>Orders</h2>
+        <Orders/>
       <h2>Moods</h2>
+        <Moods/>
       <h2>Snacks</h2>
+        <Snacks/>
       <h2>Users</h2>
+        <Users/>
     </div>
   );
 }
