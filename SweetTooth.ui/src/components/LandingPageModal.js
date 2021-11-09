8f8a201021e0170a4e9f99dc17437f055e323d17
jim-conner <jamesdavidconner@gmail.com>
2021-11-08 20:21:58 -0600

    added form to modal

--- SweetTooth.ui/src/components/LandingPageModal.js
+++ SweetTooth.ui/src/components/LandingPageModal.js
@@ -3,6 +3,7 @@ import {
   Button, Modal, ModalBody, ModalFooter, ModalHeader
 } from 'reactstrap';
 import { signInUser } from '../helpers/auth';
+import LandingPageForm from './forms/LandingPageForm';
 
 function LandingPageModal() {
   const [modal, setModal] = useState(false);
@@ -15,18 +16,19 @@ function LandingPageModal() {
     isOpen={modal} toggle={toggle}
   >
     <ModalHeader toggle={function noRefCheck() {}}>
-      Modal title
-    </ModalHeader>
-    <ModalBody>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </ModalBody>
-    <ModalFooter>
+      Start by creating your account with Google
       <Button
         color="primary"
         onClick={signInUser}
       >
         Google
       </Button>
+    </ModalHeader>
+    <ModalBody>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+    </ModalBody>
+    <LandingPageForm />
+    <ModalFooter>
       {' '}
       <Button onClick={function noRefCheck() {}}>
         Cancel
