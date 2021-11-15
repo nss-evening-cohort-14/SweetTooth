6d3eb820cccaeb5085a907abd5c0a8c79d7dbf39
jim-conner <jamesdavidconner@gmail.com>
2021-11-14 20:39:41 -0600

    added user hook, ready for .map and to get payments

--- SweetTooth.ui/src/components/UserProfile.js
+++ SweetTooth.ui/src/components/UserProfile.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Container, Row } from 'reactstrap';
 import UserAddressForm from './forms/userAddressForm';
+import PaymentMethodForm from './forms/PaymentMethodForm';
 
 export default function UserProfile({
   user, userAddresses, setUserAddresses
@@ -27,6 +28,10 @@ export default function UserProfile({
           </Container>
         ))
         }
+      <PaymentMethodForm
+        user={user}
+
+      />
     </div>
   );
 }
