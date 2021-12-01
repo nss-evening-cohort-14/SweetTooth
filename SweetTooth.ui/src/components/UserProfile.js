305da299fcc7d446e8f00e9190336ba35f85d527
jim-conner <jamesdavidconner@gmail.com>
2021-11-30 19:57:29 -0600

    updated layout and styled components

--- SweetTooth.ui/src/components/UserProfile.js
+++ SweetTooth.ui/src/components/UserProfile.js
@@ -1,32 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {
-  Container
-} from 'reactstrap';
+// import {
+//   Container
+// } from 'reactstrap';
 import PaymentMethodCard from './PaymentMethodCard';
 import PaymentMethodModal from './modals/PaymentMethodModal';
 import UserAddressModal from './modals/UserAddressModal';
 import UserAddressTable from './UserAddressTable';
-import { LandingPageContainer, LandingPageLogo } from '../styles/LandingPageStyled';
 import logo from '../Assets/SweetToothLogo.png';
+import {
+  UserPageContainer, UserPageLogo, UserInfoContainer, UserSidebar
+} from '../styles/UserPageStyled';
 
 export default function UserProfile({
   user, userAddresses, setUserAddresses, paymentMethodsArray, setPaymentMethodsArray
 }) {
   return (
-    <Container>
-        <LandingPageContainer>
-          <LandingPageLogo src={logo} />
-          </LandingPageContainer>
-          { <h4> Hi, {user.firstName}!</h4> }
-          { <h5> Update your account details here.</h5> }
-
-      <UserAddressModal
-        user={user}
-        userAddresses={userAddresses}
-        setUserAddresses={setUserAddresses}
-      />
-      <>
+    <UserPageContainer>
+      <UserInfoContainer>
       {
       userAddresses.length > 0
         ? <UserAddressTable
@@ -36,15 +27,8 @@ export default function UserProfile({
           />
         : ''
       }
-      </>
-
-      <PaymentMethodModal
-        user={user}
-        paymentMethodsArray={paymentMethodsArray}
-        setPaymentMethodsArray={setPaymentMethodsArray}
-      />
       {
-      <Container>
+      <>
         {paymentMethodsArray?.map((paymentMethodInfo) => (
           paymentMethodInfo.softDelete === false
             ? <PaymentMethodCard
@@ -57,9 +41,26 @@ export default function UserProfile({
               </PaymentMethodCard>
             : ''
         ))}
-      </Container>
+      </>
       }
-    </Container>
+      </UserInfoContainer>
+
+      <UserSidebar>
+        <UserPageLogo src={logo} />
+        { <h4> Hi, {user.firstName}!</h4> }
+        { <h5> Update your account details here.</h5> }
+        <UserAddressModal
+          user={user}
+          userAddresses={userAddresses}
+          setUserAddresses={setUserAddresses}
+        />
+        <PaymentMethodModal
+          user={user}
+          paymentMethodsArray={paymentMethodsArray}
+          setPaymentMethodsArray={setPaymentMethodsArray}
+        />
+      </UserSidebar>
+    </UserPageContainer>
   );
 }
 
--- SweetTooth.ui/src/styles/UserPageStyled.js
+++ SweetTooth.ui/src/styles/UserPageStyled.js
@@ -1,16 +1,32 @@
-// import { Card } from 'reactstrap';
 import styled from 'styled-components';
+import logo from '../Assets/SweetToothLogo.png';
 
 const UserPageContainer = styled.div`
-  display: flex;
+  display: flex
+  align-items: stretch
   justify-content: center;
-  align-items: center;
-  width: 50%;
-  padding: 10px;
-  margin: 5px auto;
+  // align-items: center;
+  // border-style: solid;
+  // box-shadow: 0 0 15px rgba(0, 0, 0, 0.5);
 `;
 
-// const InfoContainer = styled(Card)`
-// `;
+const UserInfoContainer = styled.div`
+  width: 70%
+`;
 
-export default UserPageContainer;
+const UserPageLogo = styled.img`
+  background-image: url(${logo}) no-repeat center center fixed;
+  width: 25%;
+  border-radius: 50%;
+  padding 2%;
+`;
+
+const UserSidebar = styled.div`
+  width: 30%
+  // border-style: solid;
+  // box-shadow: 0 0 15px rgba(0, 0, 0, 0.5);
+`;
+
+export {
+  UserPageContainer, UserInfoContainer, UserPageLogo, UserSidebar
+};
